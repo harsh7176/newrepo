fbf1f25dbdf0dbda23b1c71cf629f6879e128f66
harsh7176 <114111287+harsh7176@users.noreply.github.com>
2023-04-05 12:38:41 +0530

    Add files via upload

--- exdemo.py
+++ exdemo.py
@@ -1,10 +1,9 @@
 print()
-try:
- n=int(input('Enter the Numerator   : '))
- d=int(input('Enter the Denominator : '))
- q=n//d
-except:
- print('\nalert - i/o or zero division..\n')
- q=0
+
+n=int(input('Enter the Numerator   : '))
+d=int(input('Enter the Denominator : '))
+q=n//d
+print('\nalert - i/o or zero division..\n')
+q=0
 
 print('\nQuotient  : ',q)
